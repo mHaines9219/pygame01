1d2710dc73ab206c33ae36b7f21c3d1b2974bcd3
mHaines331 <mhaines9219@gmail.com>
2024-01-25 22:05:03 -0800

    all corner circles

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("blue")
+    screen.fill("black")
 
     # Update player position based on key presses
     keys = pygame.key.get_pressed()
@@ -62,18 +62,27 @@ while running:
 
     # Draw the red player circle
     pygame.draw.circle(
-        screen, "red", (int(player_pos.x), int(player_pos.y)), player_radius
+        screen, "white", (int(player_pos.x), int(player_pos.y)), player_radius
     )
 
     # Draw the green circle in the bottom-right corner
     green_circle_x = screen.get_width() - player_radius  # X position
     green_circle_y = screen.get_height() - player_radius  # Y position
     pygame.draw.circle(screen, "green", (green_circle_x, green_circle_y), player_radius)
+
     purple_circle_x = 0 + player_radius  # X position
     purple_circle_y = screen.get_height() - player_radius  # Y position
     pygame.draw.circle(
         screen, "purple", (purple_circle_x, purple_circle_y), player_radius
     )
+    yellow_circle_x = 0 + player_radius  # X position
+    yellow_circle_y = 0 + player_radius  # Y position
+    pygame.draw.circle(
+        screen, "yellow", (yellow_circle_x, yellow_circle_y), player_radius
+    )
+    red_circle_x = screen.get_width() - player_radius  # X position
+    red_circle_y = 0 + player_radius  # Y position
+    pygame.draw.circle(screen, "red", (red_circle_x, red_circle_y), player_radius)
 
     pygame.display.flip()
 
